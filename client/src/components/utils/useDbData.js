9c15544ba4eb594b16c5e411be12349f724d0a61
Jan Horak <horak.jan@live.com>
2021-05-05 20:28:30 +0200

    Addded 404 page styles, created hook for Db data

---
+++ client/src/components/utils/useDbData.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import Axios from "axios";
+
+const useDbData = (dataToLoad) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [loadedData, setLoadedData] = useState({});
+
+  useEffect(() => {
+    const getDataToLoad = async () => {
+      try {
+        const res = await Axios.get(`/api/${dataToLoad}`);
+
+        setLoadedData(res.data);
+      } catch (error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
+    };
+    getDataToLoad();
+  }, []);
+
+  console.log(dataToLoad);
+  return [loadedData, isLoading, isError];
+};
+
+export default useDbData;
