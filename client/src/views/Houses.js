219fb4222965d7d9b0d50722282ee5aec249fa26
Jan Horak <horak.jan@live.com>
2021-05-23 16:24:27 +0200

    Refactor., moved pagination to utils

--- client/src/views/Houses.js
+++ client/src/views/Houses.js
@@ -1,39 +1,17 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import HousesOnActualPage from "../components/houses/HousesOnActualPage";
-import ButtonsForPagination from "../components/houses/ButtonsForPagination";
+import house from "../basicHouseInfo.json";
+import BrowseHouses from "../components/houses/BrowseHouses";
+import FilterAndSort from "../components/houses/FilterAndSort/FilterAndSort";
+import ShowPerPage from "../components/houses/ShowPerPage";
 const Houses = () => {
-  const [actualPage, setActualPage] = useState(1);
-  // const [allHouses, setAllHouses] = useState();
-  const [perPage] = useState(3);
-  const allHouses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  const [loading, setLoading] = useState(true);
-  const [isFiltered, setIsFiltered] = useState(false);
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     const res = await axios.get("api/house");
-  //     setAllHouses(res.data);
-  //     setLoading(false);
-  //   };
-
-  //   fetchPosts();
-  // }, []);
-  // useEffect(() => {
-
-  // }, actualPage);
-
-  const lastHouse = actualPage * perPage;
-  const firstHouse = lastHouse - perPage;
-  const totalPages = Math.ceil(allHouses.length / perPage);
-  const housesOnActualPage = allHouses.slice(firstHouse, lastHouse);
-
   return (
     <div className="houses">
-      <HousesOnActualPage housesOnActualPage={housesOnActualPage} />
-      <ButtonsForPagination
-        setActualPage={setActualPage}
-        totalPages={totalPages}
-      />
+      <h1>Katalog nízkoenergetických zděných domů a dřevostaveb na klíč</h1>
+
+      <FilterAndSort />
+
+      <ShowPerPage />
+
+      <BrowseHouses house={house} />
     </div>
   );
 };
