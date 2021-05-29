6cc1d4bc4237871b3e5dc4f2733d5d8d29540d0d
Jan Horak <horak.jan@live.com>
2021-05-30 00:54:48 +0200

    New component

---
+++ client/src/components/houseDetails/HouseDetailsMiddle.js
@@ -0,0 +1,15 @@
+const HouseDetailsMiddle = () => {
+  return (
+    <div className="house-details-middle">
+      <h2>Máte vlastní představu o svém domě? </h2>
+      <h3>Upravíme Vám tento dům dle Vašich individuálních požadavků</h3>
+      <p>nebo navrhneme zcela nový dům na míru dle Vašich představ.</p>
+      <p>
+        Pomůžeme Vám od A do Z – pozemek, projekt, hypotéka, stavební povolení,
+        stavba domu, kolaudace.
+      </p>
+    </div>
+  );
+};
+
+export default HouseDetailsMiddle;
